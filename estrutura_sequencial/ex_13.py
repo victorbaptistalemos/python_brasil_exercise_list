cbfddbb97c9d14f0f1f123166e0b8ab7b05881d9
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-11-08 21:32:35 -0300

    EstruturaSequencial ex_13

---
+++ estrutura_sequencial/ex_13.py
@@ -0,0 +1,25 @@
+"""Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+ utilizando as seguintes fórmulas:
+    Para homens: (72.7*h) - 58
+    Para mulheres: (62.1*h) - 44.7"""
+
+
+def peso_ideal(h):
+    """Utiliza as fórmulas (72.7 * h) - 58 e (62.1*h) - 44.7 para calcular o peso ideal
+
+    >>> peso_ideal(1.69)
+    Peso ideal para homens: 64.863
+    Peso ideal para mulheres: 60.248999999999995
+    >>> peso_ideal(1.55)
+    Peso ideal para homens: 54.685
+    Peso ideal para mulheres: 51.55500000000001
+
+    :param h: string
+    """
+
+    print(f'Peso ideal para homens: {72.7 * float(h) - 58}')
+    print(f'Peso ideal para mulheres: {62.1 * float(h) - 44.7}')
+
+
+if __name__ == '__main__':
+    peso_ideal(input('Digite sua altura em metros ("1.70"): '))
