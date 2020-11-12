1ccbc37ed324233a99434ed5a70e761eecaa9fe6
victorbaptistalemos <victorbaptistalemos@icloud.com>
2020-11-12 20:23:05 -0300

    EstruturaDeRepeticao ex_2

---
+++ EstruturaDeRepeticao/ex_2.py
@@ -0,0 +1,24 @@
+"""Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário,
+mostrando uma mensagem de erro e voltando a pedir as informações."""
+
+
+def credenciais():
+    usuario = ''
+    senha = ''
+
+    while usuario == senha:
+        try:
+            while usuario == '':
+                usuario = input('Digite o nome do usuário: ')
+            while senha == '':
+                senha = input('Digite a senha: ')
+            if usuario == senha:
+                raise UserWarning
+        except UserWarning:
+            usuario = ''
+            senha = ''
+            print('Os nomes de usuário e senha são iguais, tente novamente')
+
+
+if __name__ == '__main__':
+    credenciais()
